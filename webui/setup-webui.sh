d7d3b2c5b743165e7b928b8915a56f5e84e00358
nodaddyno <luke@buildcollc.com>
2025-05-23 19:54:34 -0700

    bugs squashed

--- webui/setup-webui.sh
+++ webui/setup-webui.sh
@@ -29,67 +29,83 @@ cleanup() {
 
 trap cleanup EXIT
 
-# install latest nodejs
 working-dots() {
     while :; do
         echo -n "."
-        sleep .5
+        sleep .2
     done
 }
-echo -ne "\e[1;32mInstalling NodeJS..."
-working-dots &
-latest_version=$(wget -qO- https://deb.nodesource.com/ | grep -Po 'setup_\K[0-9]+(?=\.x)' | sort -nr | head -1) &> /dev/null
-bash < <(wget -qO - https://deb.nodesource.com/setup_"$latest_version".x) &> /dev/null
-apt install nodejs -y &> /dev/null
+
+# install latest nodejs
+echo -ne "\e[1;34mInstalling NodeJS..."
+working-dots & dots_pid=$!
+latest_version=$(wget -qO- https://deb.nodesource.com/ | grep -Po 'setup_\K[0-9]+(?=\.x)' | sort -nr | head -1) &> /dev/null || exit 1
+bash < <(wget -qO - https://deb.nodesource.com/setup_"$latest_version".x) &> /dev/null || exit 1
+apt install nodejs -y &> /dev/null || exit 1
 echo -e "\e[0m"
 
 # create directory structure
-echo -ne "\e[1;32mCreating directory structure..."
+echo -ne "\e[1;34mCreating directory structure..."
 mkdir /root/webui &> /dev/null
 mkdir /root/webui/node_modules &> /dev/null
 mkdir /root/webui/public &> /dev/null
 mkdir /root/webui/scripts &> /dev/null
 mkdir /root/webui/ssl &> /dev/null
 mkdir /root/webui/views &> /dev/null
-echo -e ".\e[0m"
+echo -e "\e[0m"
 
 # download files
-echo -ne "\e[1;32mDownloading files."
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/index.js" -qO /root/webui/index.js
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/launcher.sh" -qO /root/webui/launcher.sh
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/package-lock.json" -qO /root/webui/package-lock.json
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/package.json" -qO /root/webui/package.json
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/public/style.css" -qO /root/webui/public/style.css
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/about.sh" -qO /root/webui/scripts/about.sh
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/get-nord.sh" -qO /root/webui/scripts/get-nord.sh
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/login.sh" -qO /root/webui/login.sh
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/toggle-settings.sh" -qO /root/webui/scripts/toggle-settings.sh
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/views/index.ejs" -qO /root/webui/views/index.ejs
-wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/systemd-service/nord-webui.service" -qO /etc/systemd/system/nord-webui.service
+echo -ne "\e[1;34mDownloading files."
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/index.js" -qO /root/webui/index.js || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/launcher.sh" -qO /root/webui/launcher.sh || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/package-lock.json" -qO /root/webui/package-lock.json || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/package.json" -qO /root/webui/package.json || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/public/style.css" -qO /root/webui/public/style.css || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/about.sh" -qO /root/webui/scripts/about.sh || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/get-nord.sh" -qO /root/webui/scripts/get-nord.sh || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/login.sh" -qO /root/webui/login.sh || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/scripts/toggle-settings.sh" -qO /root/webui/scripts/toggle-settings.sh || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/node-server/views/index.ejs" -qO /root/webui/views/index.ejs || exit 1
+wget "https://raw.githubusercontent.com/theOtherLuke/nordlynx-router/refs/heads/main/webui/systemd-service/nord-webui.service" -qO /etc/systemd/system/nord-webui.service || exit 1
 echo -e ".\e[0m"
 
 # make scripts executable
-echo -ne "\e[1;32mMaking scripts executable..."
-chmod +x /root/webui/launcher.sh &> /dev/null
-chmod +x /root/webui/scripts/*.sh &> /dev/null
+echo -ne "\e[1;34mMaking scripts executable..."
+chmod +x /root/webui/launcher.sh &> /dev/null || exit 1
+chmod +x /root/webui/scripts/*.sh &> /dev/null || exit 1
 echo -e "\e[0m"
 
 # install node modules
-echo -ne "\e[1;32mInstalling node modules..."
-cd /root/webui &> /dev/null
-npm install ws express &> /dev/null
-echo -e ".\e[0m"
+echo -ne "\e[1;34mInstalling node modules..."
+cd /root/webui &> /dev/null || exit 1
+npm install ws express &> /dev/null || exit 1
+echo -e "\e[0m"
 
 # create self-signed certs
-echo -ne "\e[1;32mCreating self-signed certificates..."
-openssl req -nodes -new -x509 -keyout /root/webui/ssl/key.pem -out /root/webui/ssl/cert.pem &> /dev/null
-echo -e ".\e[0m"
+kill "$dots_pid" &> /dev/null
+
+read -p "Country Name (2 letter code) [US]: " C_val < /dev/tty
+C="/C=${C_val:-US}"
+read -p "State or Province Name (full name) [Some-State]: " ST_val < /dev/tty
+ST="/ST=${ST_val:-Some State}"
+read -p "Locality Name (eg, city) []:" L_val < /dev/tty
+L="/L=${L_val:-Some City}"
+read -p "Organization Name (eg, company) [Internet Widgits Pty Ltd]: " O_val < /dev/tty
+O="/O=${O_val:-MyCompany}"
+read -p "Organizational Unit Name (eg, section) []: " OU_val < /dev/tty
+OU="/OU=${OU_val:-WebUI}"
+read -p "Common Name (e.g. server FQDN or YOUR name) []: " CN_val < /dev/tty
+CN="/CN=${CN_val:-localhost}"
+
+echo -e "\e[1;34mCreating self-signed certificates...\e[1;35m"
+openssl req -nodes -new -x509 -keyout /root/webui/ssl/key.pem -out /root/webui/ssl/cert.pem -subj "${C}${ST}${L}${O}${OU}${CN}" || exit 1
 
 # enable and start the service
-echo -ne "\e[1;32mEnabling and starting node-webui.service..."
-systemctl daemon-reload &> /dev/null
-systemctl enable --now nord-webui.service &> /dev/null
-echo -e ".\e[0m"
+working-dots &
+echo -ne "\e[1;34mEnabling node-webui.service..."
+systemctl daemon-reload &> /dev/null || exit 1
+systemctl enable nord-webui.service &> /dev/null || exit 1
+echo -e "\e[0m"
 
 # byeee
 jobs -p | xargs -I{} kill {}
