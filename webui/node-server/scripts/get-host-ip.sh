43a03831744f2f0258146b08f0abf1a1ab882c42
nodaddyno <luke@buildcollc.com>
2025-05-24 11:28:53 -0700

    Update get-host-ip.sh

--- webui/node-server/scripts/get-host-ip.sh
+++ webui/node-server/scripts/get-host-ip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # check for yq and install if missing
-which yq || apt install yq -y
+which yq $> /dev/null || apt install yq -y $> /dev/null
 # Extract the first static IP from the Netplan config
 yq -r '.network.ethernets | to_entries[] | .value.addresses[0]' /etc/netplan/config-lan.yaml | cut -d'/' -f1
